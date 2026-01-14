48c9d2c23a3d0798130baa9f866a887bb79f1af3
MGlolenstine <mglolenstine@gmail.com>
2026-01-14 10:48:43 +0100

    Added AugmentCode and fixed LSP commands.

--- lua/config/remap.lua
+++ lua/config/remap.lua
@@ -3,8 +3,6 @@ vim.keymap.set('n', '<leader>n', ':Telescope find_files<CR>', { desc = 'Fuzzy fi
 vim.keymap.set('n', '<leader>b', ':Telescope buffers<CR>', { desc = 'Search through buffers' })
 vim.keymap.set('n', '<leader>h', ':Telescope help_tags<CR>', { desc = 'Search through help' })
 vim.keymap.set('n', '<leader>l', ':Telescope live_grep<CR>', { desc = 'Live grep search' })
-vim.keymap.set('n', '<leader>ss', ':Telescope lsp_document_symbols<CR>', { desc = 'Search through document symbols' })
-vim.keymap.set('n', '<leader>sS', ':Telescope lsp_workspace_symbols<CR>', { desc = 'Search through workspace symbols' })
 vim.keymap.set('v', '<leader>l', function()
     local builtin = require('telescope.builtin')
     builtin.grep_string()
@@ -12,6 +10,13 @@ end, { desc = 'Search selection' })
 
 -- LSP commands
 vim.keymap.set('n', '<leader>f', vim.lsp.buf.format, { desc = 'Format buffer' })
+vim.keymap.set('n', '<leader>ss', ':Telescope lsp_document_symbols<CR>', { desc = 'Search through document symbols' })
+vim.keymap.set('n', '<leader>ss', ':Telescope lsp_document_symbols<CR>', { desc = 'Search through document symbols' })
+vim.keymap.set('n', 'gd', ':Telescope lsp_definitions<CR>', { desc = 'Search through workspace symbols' })
+vim.keymap.set('n', 'gr', ':Telescope lsp_references<CR>', { desc = 'Search through workspace symbols' })
+vim.keymap.set("n", "K", vim.lsp.buf.hover)
+vim.keymap.set({ "n", "v" }, "<leader>a", vim.lsp.buf.code_action)
+vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename)
 
 -- Copy utils
 vim.keymap.set('v', '<leader>y', '"+y', { desc = "Copy to system clipboard" })
@@ -25,4 +30,23 @@ end, { desc = "Open file browser in current directory" })
 -- NeoVIM utils
 vim.keymap.set('n', '<leader><leader>', ':so<CR>', { desc = "Source the file" })
 
+-- UndoTree
+vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+
+-- Keybinds
+vim.keymap.set("n", "<leader>w", "<cmd>wincmd w<cr>")
+vim.keymap.set("i", "<cr>", function()
+    ---@diagnostic disable-next-line
+    local line = vim.fn.getline "."
+    local col = vim.fn.col "."
+    ---@diagnostic disable-next-line
+    local next = line:sub(col, col)
+    if vim.tbl_contains({ "}", "]" }, next) then return "<cr><esc>ko" end
+    return "<cr>"
+end, { expr = true })
+
+-- AugmentCode
+vim.keymap.set({"v", "n"}, "<leader>ac", ":Augment chat<CR>")
+vim.keymap.set("n", "<leader>an", ":Augment chat-new<CR>")
+vim.keymap.set("n", "<leader>at", ":Augment chat-toggle<CR>")
 
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,79 @@
+local function picker(name, config)
+  return function() require "snacks.picker"[name](config) end
+end
+
+return {
+  "folke/snacks.nvim",
+  lazy = false,
+  tag = "v2.22.0",
+  priority = 1000,
+  opts = {
+    lazygit = { enabled = true },
+    notifier = { enabled = true },
+    terminal = { enabled = false },
+    input = { enabled = false },
+    toggle = { enabled = false },
+    bigfile = {
+      enabled = true,
+      notify = true,
+      size = 2 * 1024 * 1024,
+      line_length = 100000,
+    },
+    image = {
+      enabled = true,
+      force = true,
+    },
+    picker = {
+      enabled = true,
+      ui_select = true,
+      main = {
+        file = false,
+      },
+      icons = {
+        files = {
+          enabled = false,
+        },
+      },
+      win = {
+        input = {
+          keys = {
+            ["<CR>"] = { "confirm", mode = { "n", "i" } },
+            ["<Down>"] = { "list_down", mode = { "n", "i" } },
+            ["<Up>"] = { "list_up", mode = { "n", "i" } },
+            ["<Esc>"] = { "close", mode = { "n", "i" } },
+            ["<c-p>"] = { "select_and_prev", mode = { "n", "i" } },
+            ["<c-n>"] = { "select_and_next", mode = { "n", "i" } },
+            ["<TAB>"] = { "list_down", mode = { "n", "i" } },
+            ["<S-TAB>"] = { "list_up", mode = { "n", "i" } },
+          },
+        },
+        list = {
+          keys = {
+            ["<CR>"] = "confirm",
+            ["<Down>"] = "list_down",
+            ["<Esc>"] = "close",
+            ["<c-p>"] = { "select_and_prev", mode = { "n", "x" } },
+            ["<c-n>"] = { "select_and_next", mode = { "n", "x" } },
+            ["<Up>"] = "list_up",
+            ["<TAB>"] = "list_down",
+            ["<S-TAB>"] = "list_up",
+          },
+        },
+      },
+    },
+  },
+  keys = {
+    {
+      "<leader>e",
+      function()
+        if not vim.diagnostic.open_float() then
+          picker "diagnostics_buffer" ()
+        end
+      end,
+    },
+    { "<leader>E", picker "diagnostics" },
+    { "<leader>g", function() require "snacks".lazygit() end },
+    { "<leader>G", function() require "snacks".git.blame_line() end },
+  },
+}
+
